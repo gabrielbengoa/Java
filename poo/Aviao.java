c2a687bbe4235a715c4d6037666a52d9922d343e
gabrielbengoa <codsuckers@gmail.com>
2020-05-15 13:56:15 -0300

    Adição das atividades de POO (Programação Orientada a Objetos)

---
+++ poo/Aviao.java
@@ -0,0 +1,16 @@
+// @author Gabriel Barros Bengoa
+
+package poo;
+
+public class Aviao extends Carro {
+    double envergadura;
+    
+    void aterrisar() {
+    	System.out.println("---------______________");
+    	
+    }
+    
+    void acelerar() {
+    	System.out.println("_________----------");
+    }
+}
